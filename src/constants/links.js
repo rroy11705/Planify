34eee867c50f50f81cf0699e5fd2e926139f79c1
Rahul Roy <rroy11705@gmail.com>
2024-12-23 12:56:26 +0530

    first commit

---
+++ src/constants/links.js
@@ -0,0 +1,4 @@
+export const PRIVACY_POLICY_LINK =
+  'https://www.freeprivacypolicy.com/live/46086875-7494-4209-91a0-3266e63600ff';
+export const TERMS_CONDITIONS_LINK =
+  'https://www.freeprivacypolicy.com/live/891e890d-ab88-42e7-87a1-4125be04e83c';
